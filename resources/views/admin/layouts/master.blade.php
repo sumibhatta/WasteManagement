34a20dc2a296d8b26763f3fff914dc75d581dd3f
sumibhatta <sumibhatta37@gmail.com>
2022-10-28 09:54:27 -0400

    footer link fix

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -54,7 +54,7 @@
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+          Copyright &copy; <?php echo date("Y"); ?> <div class="bullet"></div>Waste Management</a>
         </div>
         <div class="footer-right">
           
